9e449f1c863c83a668ad800761930e91aba1f38a
katrox <wildcityserver@qq.com>
2024-06-21 12:09:29 +0800

    chore: 新增依赖 `unplugin-auto-import` 与 `unplugin-vue-components`

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    AutoImport({
+      imports: ['vue'],
+      resolvers: [NaiveUiResolver()],
+      dts: './auto-imports.d.ts',
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
+  ],
   server: {
     host: '0.0.0.0',
   },
