01ce1ef4af991223f2b7182e64365ef667f08397
katrox <wildcityserver@qq.com>
2024-06-24 10:05:34 +0800

    feat: 主题颜色跟随系统

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
   import { ArrowDown } from '@vicons/ionicons5'
   import type Content from './components/Content.vue'
+  import { useOsTheme, darkTheme } from 'naive-ui'
 
+  const osThemeRef = useOsTheme()
+  const theme = computed(() => (osThemeRef.value == 'dark' ? darkTheme : null))
   const contentRef = ref<InstanceType<typeof Content>>()
 
   function clearMessages() {
@@ -17,13 +20,16 @@
 </script>
 
 <template>
-  <div class="max-w-70ch mx-a pt-24 pb-16 px-8">
-    <n-message-provider>
-      <Header @response="clearMessages" class="mb-4" />
-      <Content ref="contentRef" />
-      <n-float-button @click="scroll2Bottom" top="20" right="20" shape="square">
-        <n-icon><arrow-down /></n-icon>
-      </n-float-button>
-    </n-message-provider>
-  </div>
+  <n-config-provider :theme="theme">
+    <n-global-style />
+    <div class="max-w-70ch mx-a pt-24 pb-16 px-8">
+      <n-message-provider>
+        <Header @response="clearMessages" class="mb-4" />
+        <Content ref="contentRef" />
+        <n-float-button @click="scroll2Bottom" top="20" right="20" shape="square">
+          <n-icon><arrow-down /></n-icon>
+        </n-float-button>
+      </n-message-provider>
+    </div>
+  </n-config-provider>
 </template>
